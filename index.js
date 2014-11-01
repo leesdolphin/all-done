4b970ee8af17512c2115b2859d54ce2c862e21c0
Lee Symes <leesdolphin@gmail.com>
2014-11-01 23:11:46 +1000

    Prevent unlock from firing the callback if called multiple times in a row.
    Version bump to 0.1.1

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-
-module.export = function all(callback_fn) {
-    var queueCount = 0;
+module.export = function all_done(callback_fn) {
+    var queue_count = 0;
     var locked = false;
 
-    var makeCallback = function (wrapped_callback) {
-        queueCount++;
+    var make_callback = function (wrapped_callback) {
+        queue_count++;
+        console.log("Creating callback. Have ", queue_count, " pending");
         return function () {
             if (wrapped_callback)
                 wrapped_callback.apply(this, arguments);
-            queueCount--;
-            if (!locked && queueCount <= 0) {
+            queue_count--;
+            if (!locked && queue_count <= 0) {
                 callback_fn();
             }
         };
     };
 
-    makeCallback.lock = function () {
+    make_callback.lock = function () {
         locked = true;
         return this;
     };
-    makeCallback.unlock = function () {
-        locked = false;
-        if (queueCount <= 0) {
-            console.log("All done");
+    make_callback.unlock = function () {
+        if (locked && queue_count <= 0) {
+            // This prevents unlock from calling the callback if we weren't locked.
             callback_fn();
         }
+        locked = false;
         return this;
     };
 
-    return makeCallback;
+    return make_callback;
 };
